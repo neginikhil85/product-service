2ddb0487bced0beb9308062201f4c4dd1dbbd337
Nikhilnegi85 <neginikhil85@gmail.com>
2022-12-19 12:09:24 +0530

    initial commit

---
+++ src/main/java/com/learning/respository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.learning.respository;
+
+import com.learning.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
